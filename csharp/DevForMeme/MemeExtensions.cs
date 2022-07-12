c990863cc1073ee02015277282b8ae8393a8b7cf
Nattapong Nunpan <aixasz@hotmail.com>
2022-07-12 17:58:26 +0700

    initial meme for csharp .net

---
+++ csharp/DevForMeme/MemeExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace DevForMeme;
+
+/// <summary>
+/// Meme driven development.
+/// </summary>
+public static class MemeExtensions
+{
+    /// <summary>
+    /// YOLO : Your object life once.
+    /// </summary>
+    public static IServiceCollection Yolo<TService, TImplementation>(this IServiceCollection services)
+        where TService : class
+        where TImplementation : class, TService
+    {
+        if (services == null)
+        {
+            throw new ArgumentNullException(nameof(services));
+        }
+
+        return services.AddSingleton(typeof(TService), typeof(TImplementation));
+    }
+}
